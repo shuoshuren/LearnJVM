3ea211ad4bbc4bf70618183557145c0a1fb32b4f
shuoshuren <534361124@qq.com>
2020-04-15 00:09:44 +0800

    常量字段描述

--- src/main/java/com/xc/jvm/bytecode/MyTest1.java
+++ src/main/java/com/xc/jvm/bytecode/MyTest1.java
@@ -17,6 +17,15 @@ package com.xc.jvm.bytecode;
  * 是满足某些常量池索引值的数据在特定情况下需要表达[不引用任何一个常量池]的含义;根本原因在于,索引0也是一个常量(保留常量),
  * 只不过它不位于常量表中,这个常量就对应null值,所以,常量池的索引从1而非0开始
  *
+ * 6.在JVM规范中,每个变量/字段都有描述信息,描述信息主要的作用是描述字段的数据类型,方法的参数列表(包括数据,类型,与顺序)与返回值.根据
+ * 描述符规则,基本数据类型和代码无返回值的void类型都用一个大写字符来表示,对象类型则使用字符L加对象的全限定名称来表示,为压缩字节码
+ * 体积,对基本数据类型,jvm都只用一个大写字母来表示: B - byte, C - char, D - double, F - float,I - int,
+ * J - long, S - short, Z - boolean, V - void, L - 对象类型
+ *
+ * 7.对于数组类型来说,每一个维度使用一个前置的[来表示,如int[] 被标记为[I,String[][]被标记为[[Ljava/lang/String;
+ * 8.用描述符来描述方法是,按照先参数列表,后返回值的顺序来描述.参数列表按照参数的严格顺序放在一组()之内,如方法:String getRealNameByIdAndNickName(
+ * int id,String name)的描述符为:(I,Ljava/lang/String;)Ljava/lang/String;
+ *
  *
  */
 public class MyTest1 {
